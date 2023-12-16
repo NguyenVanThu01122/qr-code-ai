b7b1fa791cfb3113e90da50362e22c36406ffca4
nguyen van thu <vanthu01122@gmail.com>
2023-12-16 22:42:10 +0700

    done

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'pbxt.replicate.delivery',
+      'g4yqcv8qdhf169fk.public.blob.vercel-storage.com',
+      'hpxpp3nxofbhdh0y.public.blob.vercel-storage.com'
+    ],
+  },
+};
+
+module.exports = nextConfig;
